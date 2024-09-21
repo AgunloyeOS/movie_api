a38141ffaa3cccfe08121c3f8fb000f968762a66
Oluwatosin Agunloye <oluwatosinagunloye@gmail.com>
2024-09-21 19:41:22 +0100

    Fix import paths in models.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Text
 from sqlalchemy.orm import relationship
-from app.database import Base
+from database import Base
+#from app.database import Base
 
 class User(Base):
     __tablename__ = "users"
